aa99fdbc61a0c1764cf5a2aa6881b8d665b72ef2
JulioMoreno1 <158115372+JulioMoreno1@users.noreply.github.com>
2024-06-05 20:51:08 -0600

    Start the proyect

---
+++ src/main/java/Logic/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logic;
+
+/**
+ *
+ * @author jigsaw
+ */
+public class Employee {
+    
+    private String firstname;
+    private String lastname;
+    private String Address;
+    
+}
